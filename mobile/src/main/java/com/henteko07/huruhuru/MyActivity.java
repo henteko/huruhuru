3edeca887b1a69368b68d712e6b28157f0cf21d6
henteko <henteko07@gmail.com>
2014-08-06 00:07:32 +0900

    シェイク出来るようにした

--- mobile/src/main/java/com/henteko07/huruhuru/MyActivity.java
+++ mobile/src/main/java/com/henteko07/huruhuru/MyActivity.java
@@ -3,17 +3,23 @@ package com.henteko07.huruhuru;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
+
+import com.squareup.seismic.ShakeDetector;
 
 
-
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements ShakeDetector.Listener {
+    private static boolean mIsSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,10 @@ public class MyActivity extends Activity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        ShakeDetector sd = new ShakeDetector(this);
+        sd.start(sensorManager);
     }
 
 
@@ -46,6 +56,12 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void hearShake() {
+        if (mIsSwitch) {
+            Log.d("shake!!!!!!!!!", "shake!!!!!!!now!!!!!!!!!!!");
+        }
+    }
+
     /**
      * A placeholder fragment containing a simple view.
      */
@@ -59,6 +75,21 @@ public class MyActivity extends Activity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_my, container, false);
             return rootView;
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            mIsSwitch = false;
+
+            ToggleButton button = (ToggleButton) getActivity().findViewById(R.id.toggleButton);
+            button.setChecked(mIsSwitch);
+            button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    mIsSwitch = isChecked;
+                }
+            });
         }
     }
 }
